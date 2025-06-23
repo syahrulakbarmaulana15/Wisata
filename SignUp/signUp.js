861bcda60500d1817b4b0b89fe0200d9b73215ee
Syahrul Akbar Maulana <syahrulakbarmaulana2000.com>
2025-06-23 07:30:41 +0700

    Desain selesai

---
+++ SignUp/signUp.js
@@ -0,0 +1,36 @@
+let container = document.getElementById("container")
+let formData= document.querySelector("form")
+let message = document.getElementById("shortmsg")
+let userData=JSON.parse(localStorage.getItem("userDatabase")) ||[];
+formData.addEventListener("submit", function(e){
+    e.preventDefault();
+let mail = document.getElementById("email").value
+let name = document.getElementById("fname").value + " " +document.getElementById("lname").value;
+let password =document.getElementById("password").value;
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email.trim());
+  }
+  if (!validateEmail(mail)) {
+    message.innerHTML = 'Please enter a valid email address.';
+    return;
+  }
+let userCrd={
+                name : name,
+                email: mail,
+                password: password
+            }
+    userData.push(userCrd)
+
+localStorage.setItem("userDatabase",JSON.stringify(userData))
+message.innerHTML="Sign Up Successful..."
+setTimeout(()=>window.location.href="../SignIn/SignIn.html",2000)
+})
+
+
+// let n= document.getElementById("google");
+// n.addEventListener("click",go);
+
+// async function go(){
+//     window.location.href="#"
+// }
